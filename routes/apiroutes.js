d99cd7af4c505310beb1a1cf26366dc87321c49c
JudyMotha <judyxaviermotha@gmail.com>
2021-09-09 09:13:47 -0400

    Update apiroutes.js

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -6,7 +6,7 @@ const db = require('../models');
 
 //dB name  workout -table /collection is  workouts-Look at Robo3T on local machine and login to mongo atlas to see collections likewise for db /table update .Refer Network for routes
 //Bootcamp is coming to closing stage Week 18 Class contains has mongo atlas set up and Heroku deployment instrucions ...where to use you password etc  in final stages
-
+//addfield ,sum,aggregate were on README for HW
 //The $addFields stage is equivalent to a $project stage that explicitly specifies all existing fields in the input documents and adds the new fields
 //https://docs.mongodb.com/manual/reference/operator/aggregation/addFields/ {-id way of solving also  works};{ $sum: <expression> };$sum is available in these stages:
 //$addFields (Available starting in MongoDB 3.4).REfer example with db.sales.aggregate
